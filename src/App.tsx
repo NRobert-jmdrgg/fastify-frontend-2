dcace61b9c9454a745699a91dfa0f5a603e5f868
Róbert Nagy <rob.iskola1@gmail.com>
2022-07-28 17:23:21 +0200

    top bar navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,13 @@ import {
   Route,
   Navigate,
 } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import getData from './utils/getData';
 import TopBar from './components/top-bar.component';
-import MoviePagination from './components/pagination.component';
 
 function App() {
   return (
     <>
-      <TopBar />
       <Router>
+        <TopBar />
         <Routes>
           <Route path='/' element={<Navigate replace to={'/page/1'} />} />
           <Route path='/page/:num' element={<Movies />} />
